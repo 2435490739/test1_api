31866339ab69257f1407c61c88c236d408023d4e
jonas <848429811@qq.com>
2020-10-23 10:56:30 +0800

    提交三次

---
+++ request1.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+import requests
+import json
+
+s = requests.session()
+
+url = 'http://luxury-mall-dev.ssluxury.cn/api-u/user/phonePasswordNoCode'
+data = {"phone": "18296637285", "password": "a123456"}
+headers = {"Content-Type": "application/json"}
+
+r =s.post(url=url,data=json.dumps(data),headers=headers)
+print r.text
